2995df2b03c78d1b19bf97d530fc2710bbbe1670
Yaroslav Balanovskiy <ybalanovskiy@yahoo.com>
2017-08-01 22:55:02 +0300

    Added text in EditText

--- app/src/main/java/com/ybalanovskiy/guiexam/MainActivity.java
+++ app/src/main/java/com/ybalanovskiy/guiexam/MainActivity.java
@@ -5,25 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private int iClkTime = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        final String prefixPrompt = "This no";
-        final String suffixPrompt = "Time (s)";
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setText("Some text)))");
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            
-            public void onClick(View v) {
-                Log.d("ProgTraceInfo", prefixPrompt + (iClkTime++) + suffixPrompt);
-            }
-        });
+
+
     }
 }
