c59ebc1068b3d886a9d629230366312bb84a6594
Yaroslav Balanovskiy <ybalanovskiy@yahoo.com>
2017-08-01 22:46:34 +0300

    Added onClickListener

---
+++ app/src/androidTest/java/com/ybalanovskiy/guiexam/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.ybalanovskiy.guiexam;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Root on 01.08.2017.
+ */
+public class MainActivityTest {
+
+}
--- app/src/main/java/com/ybalanovskiy/guiexam/MainActivity.java
+++ app/src/main/java/com/ybalanovskiy/guiexam/MainActivity.java
@@ -2,17 +2,28 @@ package com.ybalanovskiy.guiexam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private int iClkTime = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = new TextView(this);
-        tv.setText("Hello World!!!");
-        setContentView(tv);
+        Button button = (Button) findViewById(R.id.button);
+        final String prefixPrompt = "This no";
+        final String suffixPrompt = "Time (s)";
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            
+            public void onClick(View v) {
+                Log.d("ProgTraceInfo", prefixPrompt + (iClkTime++) + suffixPrompt);
+            }
+        });
     }
 }
